048a3a07657fe531941854ce08a2c96c2e7f9e99
Jeremias Erbs <jerry@badabam.de>
2015-11-06 23:36:04 +0100

    Minor cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,17 @@ var sass = require('gulp-sass');
 var del  = require('del');
 var rename = require('gulp-rename');
 
+// start development
+gulp.task('default', ['dev']);
+
+// start scss watch mode
+gulp.task('dev', ['sass'], function () {
+  gulp.watch('source/**/*.scss', ['sass']);
+});
+
+// create normal and minified versions
+gulp.task('build', ['clean', 'sass', 'sass-min']);
+
 // create readable css from scss files
 gulp.task('sass', function () {
   return gulp.src('source/*.scss')
@@ -24,11 +35,4 @@ gulp.task('sass-min', function () {
 // delete dist folder
 gulp.task('clean', function () {
   return del(['dist']);
-});
-
-gulp.task('default', ['dev']);
-gulp.task('build', ['clean', 'sass', 'sass-min']);
-
-gulp.task('dev', ['sass'], function () {
-  gulp.watch('source/**/*.scss', ['sass']);
 });
